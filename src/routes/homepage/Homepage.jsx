64ab8e50e45f35aa12d0eee968ed61b093b1d844
Daniel Baruffa <dani.baruffa@gmx.ch>
2025-11-12 16:22:20 +0100

    Remove second column from homepage

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -4,13 +4,9 @@ import './homepage.css'
 const Homepage = () => {
     return (
         <div className='homepage'>
-            {/*<img src="..." alt="", className="orbital" />*/}
-            <div className="left">
-                <h1>ChartAI</h1>
-                <h2>Create charts :D</h2>
-                <Link to="/dashboard">Get Started</Link>
-            </div>
-            <div className="right">...</div>
+            <h1>ChartAI</h1>
+            <h2>Create charts :D</h2>
+            <Link to="/dashboard">Get Started</Link>
         </div>
     )
 }
