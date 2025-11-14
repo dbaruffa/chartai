e7d51dba3383b6d10f57be6ad3dd0432fc6aeaab
Daniel Baruffa <dani.baruffa@gmx.ch>
2025-11-14 14:28:44 +0100

    Remove options from dashboard

--- src/routes/dashboardPage/DashboardPage.jsx
+++ src/routes/dashboardPage/DashboardPage.jsx
@@ -40,11 +40,6 @@ const DashboardPage = () => {
                     <img src="/logo.png" alt="" />
                     <h1>CHART AI</h1>
                 </div>
-                <div className="options">
-                    <div className='option'><span>Create a new chat</span></div>
-                    <div className='option'><span>Analyze image</span></div>
-                    <div className='option'><span>Help me with my code</span></div>
-                </div>
             </div>
             <div className='formContainer'>
                 <form onSubmit={createNewChat}>
