d17a8025393b28220588b1448103ad6189c1bda7
Daniel Baruffa <dani.baruffa@gmx.ch>
2025-11-19 19:20:28 +0100

    prompt: Initial basic system prompt

--- src/lib/prompts.js
+++ src/lib/prompts.js
@@ -1,3 +1,3 @@
-const systemPrompt = null;
+const systemPrompt = `You are ChartAI, an expert in data visualization. Your goal is to write code to visualize data in a clear and concise way.`;
 
 export { systemPrompt };
